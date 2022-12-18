7bbc154c01df48f06a30413c3046fa2c8e61b96b
kkwondev <kkwoncokr@gmail.com>
2022-12-18 12:53:58 +0900

    add redux

---
+++ AppInner.tsx
@@ -0,0 +1,52 @@
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {useSelector} from 'react-redux';
+import {RootStackParamList} from './App';
+import Delivery from './src/pages/Delivery';
+import Orders from './src/pages/Orders';
+import Settings from './src/pages/Settings';
+import SignIn from './src/pages/SignIn';
+import SignUp from './src/pages/SignUp';
+import {RootState} from './src/store/reducer';
+
+const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+function AppInner() {
+  const isLoggedIn = useSelector((state: RootState) => !!state.user.email);
+
+  return isLoggedIn ? (
+    <Tab.Navigator>
+      <Tab.Screen
+        name="Orders"
+        component={Orders}
+        options={{title: '오더 목록'}}
+      />
+      <Tab.Screen
+        name="Delivery"
+        component={Delivery}
+        options={{headerShown: false}}
+      />
+      <Tab.Screen
+        name="Settings"
+        component={Settings}
+        options={{title: '내 정보'}}
+      />
+    </Tab.Navigator>
+  ) : (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="SignIn"
+        component={SignIn}
+        options={{title: '로그인'}}
+      />
+      <Stack.Screen
+        name="SignUp"
+        component={SignUp}
+        options={{title: '회원가입'}}
+      />
+    </Stack.Navigator>
+  );
+}
+
+export default AppInner;
